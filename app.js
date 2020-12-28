8adb3a283993578bcf303cdcda9fc1e71d40d793
MalbonaGenio <pvalverdemolina@gmail.com>
2020-12-28 08:48:29 +0000

    added event listener for the submint button in the form or the enter key. we store the values of title and text and check if at least one has contenct before adding the new note. The note is stored in an array notes that contains objects with the properties title, text, color and id. To add the note we spread the notes in the array notes and add the new note at the end so we dont mutate the array.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 class App {
   constructor() {
     //the this.$form is to indicate that we are refering to an html element for data we will use this.data
+
+    //data elements
+    this.notes = []
+    //HTML elements
     this.$form = document.querySelector('#form')
     this.$noteTitle = document.querySelector('#note-title')
+    this.$noteText = document.querySelector('#note-text')
     this.$formButtons = document.querySelector('#form-buttons')
 
     this.addEventListeners() //want to make sure we run the method when we create the app
@@ -12,6 +17,18 @@ class App {
   addEventListeners() {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event)
+    })
+
+    //event listener for the submit of the form, includes the button and enter key
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault() //prevents default reload of the page
+      const title = this.$noteTitle.value
+      const text = this.$noteText.value
+      const hasNote = title || text
+      if (hasNote) {
+        //instead of passing to arguments we pass a single object so if we dont remember the exact order we will still be able to get the data correctly. This shorthand is equal to write title: title.
+        this.addNote({ title, text }) //
+      }
     })
   }
 
@@ -31,6 +48,18 @@ class App {
     this.$form.classList.remove('form-open')
     this.$noteTitle.style.display = 'none'
     this.$formButtons.style.display = 'none'
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: 'white',
+      //sets the id of the note based on the amount of notes in the notes array
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    }
+
+    this.notes = [...this.notes, newNote] //spreads the array notes and adds the newNote at the end
   }
 }
 
