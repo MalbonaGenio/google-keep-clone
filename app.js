9087735dd16a398e718f2bc042bc6fd6838c1c25
MalbonaGenio <pvalverdemolina@gmail.com>
2020-12-28 08:01:08 +0000

    added the addEventListeners method for document.body so we can use it for the handleFormClick method. Also added it to the constructor so it runs when the app is created.
    Using query selector we reference the form, noteTitle and formbuttons html id elements in the constuctor.
    
    on the handleFormClick we store the true/false value of the event.target to check if there is a click. when true the new note form expanse bc we add the form-open class and be display as block the noteTitle and the formButtons. when false remove the open class and set the dsplay of buttons and title to none.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 class App {
   constructor() {
-    console.log('app works!')  
-  }    
+    //the this.$form is to indicate that we are refering to an html element for data we will use this.data
+    this.$form = document.querySelector('#form')
+    this.$noteTitle = document.querySelector('#note-title')
+    this.$formButtons = document.querySelector('#form-buttons')
+
+    this.addEventListeners() //want to make sure we run the method when we create the app
+  }
+
+  //define the eventListenrs for the app
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      this.handleFormClick(event)
+    })
+  }
+
+  handleFormClick(event) {
+    //checks if the form elemtn is the target of the click
+    const isFormClicked = this.$form.contains(event.target)
+    isFormClicked ? this.openForm() : this.closeForm()
+  }
+
+  openForm() {
+    this.$form.classList.add('form-open')
+    this.$noteTitle.style.diplay = 'block'
+    this.$formButtons.style.display = 'block'
+  }
+
+  closeForm() {
+    this.$form.classList.remove('form-open')
+    this.$noteTitle.style.display = 'none'
+    this.$formButtons.style.display = 'none'
+  }
 }
 
 new App()
