70f4a9a6d7d9edf46445c46e3cae704a6c14f4e8
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2020-12-31 09:31:59 +0900

    new notes are now dispalyed Added the function displayNotes. Checks if there is content on the notes array, if there is takes out the place holder. After that use map to go trought the notes in the array and returns html code that is inserted in the notes div. Used .join to eliminate the comma.

--- app.js
+++ app.js
@@ -5,22 +5,24 @@ class App {
     //data elements
     this.notes = []
     //HTML elements
-    this.$form = document.querySelector('#form')
-    this.$noteTitle = document.querySelector('#note-title')
-    this.$noteText = document.querySelector('#note-text')
-    this.$formButtons = document.querySelector('#form-buttons')
+    this.$form = document.querySelector("#form")
+    this.$noteTitle = document.querySelector("#note-title")
+    this.$noteText = document.querySelector("#note-text")
+    this.$formButtons = document.querySelector("#form-buttons")
+    this.$placeHolder = document.querySelector("#placeholder")
+    this.$notes = document.querySelector("#notes")
 
     this.addEventListeners() //want to make sure we run the method when we create the app
   }
 
   //define the eventListenrs for the app
   addEventListeners() {
-    document.body.addEventListener('click', event => {
+    document.body.addEventListener("click", (event) => {
       this.handleFormClick(event)
     })
 
     //event listener for the submit of the form, includes the button and enter key
-    this.$form.addEventListener('submit', event => {
+    this.$form.addEventListener("submit", (event) => {
       event.preventDefault() //prevents default reload of the page
       const title = this.$noteTitle.value
       const text = this.$noteText.value
@@ -39,27 +41,51 @@ class App {
   }
 
   openForm() {
-    this.$form.classList.add('form-open')
-    this.$noteTitle.style.display = 'block'
-    this.$formButtons.style.display = 'block'
+    this.$form.classList.add("form-open")
+    this.$noteTitle.style.display = "block"
+    this.$formButtons.style.display = "block"
   }
 
   closeForm() {
-    this.$form.classList.remove('form-open')
-    this.$noteTitle.style.display = 'none'
-    this.$formButtons.style.display = 'none'
+    this.$form.classList.remove("form-open")
+    this.$noteTitle.style.display = "none"
+    this.$formButtons.style.display = "none"
   }
 
   addNote(note) {
     const newNote = {
       title: note.title,
       text: note.text,
-      color: 'white',
+      color: "white",
       //sets the id of the note based on the amount of notes in the notes array
-      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     }
 
-    this.notes = [...this.notes, newNote] //spreads the array notes and adds the newNote at the end
+    this.notes = [...this.notes, newNote] //spreads the array notes and adds the newNote at the end, this way we update the array without mutating the original, we will preserve all the old notes stored there.
+    this.displayNotes()
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0
+    this.$placeHolder.style.display = hasNotes ? "none" : "flex"
+    //using .map iterates trough the notes array and returns all the following html to the new notes to display.
+    //Setting the innerhtml to the .map will populate the div notes with all the generated code with each note data.
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
+        <div style="background: ${note.color};" class="note">
+          <div class="${note.title && "note-title"}">${note.title}</div>
+          <div class="note-text">${note.text}</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+     `
+      )
+      .join("") //Transforms the array into a string and bc no separator is defined the , between notes is not displayed anymore.
   }
 }
 
